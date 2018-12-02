2fdeacbb8c9097eb8ec82c060fa8f412afcbb313
Richard Pasek <richard.pasek@gmail.com>
2018-12-01 23:32:35 -0500

    Exporting project to makefiles

---
+++ trami.cydsn/app_source.mk
@@ -0,0 +1,11 @@
+#This abstracts the application source files selected by the user.
+
+APP_C_SOURCE_CortexM0=\
+	main.c\
+	BLEApplications.c\
+	HandleLowPower.c
+
+APP_ASM_SOURCE_CortexM0=\
+
+APP_LIBS_CortexM0=\
+
